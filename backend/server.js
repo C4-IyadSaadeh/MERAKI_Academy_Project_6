51e50f55c1aa59f710128e3aff1c129a7588ebd9
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-03-11 00:02:13 +0300

    Add Routes to Server

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,28 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = 5000;
+//====================================================//import database
+const connection = require("./database/db");
+
+//====================================================// Import Routers
+const AdminRouter = require("./routes/Admin");
+const MovieRouter = require("./routes/Movie");
+const GenreRouter = require("./routes/Genre");
+const RoleRouter = require("./routes/Role");
+const SubscriptionRouter = require("./routes/Subscription");
+const UserRouter = require("./routes/User")
+app.use(express.json());
+
+//====================================================// Routes Middleware
+app.use("/admin", AdminRouter);
+app.use("/movie", MovieRouter);
+app.use("/Genre", GenreRouter);
+app.use("/role", RoleRouter);
+app.use("/subscription", SubscriptionRouter);
+app.use("/user", UserRouter);
+
+//====================================================// Handles any other endpoints [unassigned - endpoints]
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 app.listen(PORT, () => {
   console.log(`SERVER WORKING ON PORT: ${PORT}`);
