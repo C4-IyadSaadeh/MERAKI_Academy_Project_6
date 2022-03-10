92453f0376eba91d7adb4c91844149261681ad1d
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-03-10 23:58:27 +0300

    Finish Genre Route

---
+++ backend/routes/Genre.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const GenreRouter = express.Router();
+const {
+    CreateNewGenre,
+    getAllGenre,
+    updateGenreById,
+    deleteGenreById,
+} = require('../controllers/Genre');
+
+GenreRouter.post('/createGenre', CreateNewGenre);
+GenreRouter.get('/all', getAllGenre);
+GenreRouter.put('/updateGenreById', updateGenreById);
+GenreRouter.delete('/deleteGenreById', deleteGenreById);
+module.exports = GenreRouter;
