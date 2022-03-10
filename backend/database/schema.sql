aacb89d382e15cbef731586e62edd94b73253d8e
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-03-10 20:54:18 +0300

    Create DataBase

---
+++ backend/database/schema.sql
@@ -0,0 +1,64 @@
+Drop Database if exists NetFlex;
+
+CREATE Database NetFlex;
+
+USE NetFlex;
+
+CREATE Table Genree(
+    id int not null auto_increment primary key,
+    title VARCHAR(255) NOT NULL,
+    is_deleted TINYINT Default 0
+);
+
+Create Table Movie(
+    id int not null auto_increment primary key,
+    title VARCHAR(255) NOT NULL,
+    duration decimal NOT NULL,
+    rate int NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    quality VARCHAR(255) NOT NULL,
+    posterImg VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    years VARCHAR(255) NOT NULL,
+    cast VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    trailer VARCHAR(255) NOT NULL,
+    genre int NOT NULL,
+    is_deleted TINYINT Default 0,
+    FOREIGN KEY (genre) REFERENCES Genree(id)
+);
+
+CREATE Table Subscription(
+    id int not null auto_increment primary key,
+    title VARCHAR(255) NOT NULL,
+    price VARCHAR(255) NOT NULL,
+    quality VARCHAR(255) NOT NULL,
+    watch VARCHAR(255) NOT NULL,
+    is_deleted TINYINT Default 0
+);
+
+CREATE Table Role(
+    id int not null auto_increment primary key,
+    title VARCHAR(255) NOT NULL,
+    is_deleted TINYINT Default 0
+);
+
+CREATE Table User(
+    id int not null auto_increment primary key,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    plan int NOT NULL,
+    role int NOT NULL,
+    is_deleted TINYINT Default 0,
+    FOREIGN KEY (plan) REFERENCES Subscription(id),
+    FOREIGN KEY (role) REFERENCES Role(id)
+);
+
+Create Table Admin (
+    id int not null auto_increment primary key,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role int NOT NULL,
+    is_deleted TINYINT Default 0,
+    FOREIGN KEY (role) REFERENCES Role(id)
+);
