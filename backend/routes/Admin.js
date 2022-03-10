7a6fb58352182e48b07ac7517be06688460f876a
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-03-10 23:58:42 +0300

    Finish Admin Route

---
+++ backend/routes/Admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const AdminRouter= express.Router();
+const {
+    CreateNewAdmin,
+    getAllAdmin,
+    updateAdminById,
+    deleteAdminById,
+  }=require('../controllers/Admin');
+  const {
+  
+    loginAdmin
+  }= require('../controllers/Login')
+  AdminRouter.post('/createAdmin',CreateNewAdmin);
+AdminRouter.post('/loginAdmin',loginAdmin);
+  AdminRouter.get('/all',getAllAdmin);
+  AdminRouter.put('/updateAdminById',updateAdminById);
+  AdminRouter.delete('/deleteAdminById',deleteAdminById);
+module.exports =AdminRouter;
