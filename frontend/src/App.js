e337e0cf6805f640ace711dc9264185765ee2477
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-03-16 22:46:15 +0300

    Add Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,24 @@ import "./App.css";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Plan from "./Components/Plan/Plan";
+import Home from "./Components/Home/Home";
+import SingleMovie from "./Components/SingleMovie/SingleMovie";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import FullVideo from "./Components/SingleMovie/FullVideo";
+import HeaderOne from "./Components/Header/HeaderOne"
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
       {/* <Register/> */}
-<Plan/>
+{/* <Plan/> */}
+<HeaderOne/>
+<Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/FullMovie/:id" element={<FullVideo/>} />
+
+        </Routes>
+
     </div>
   );
 }
