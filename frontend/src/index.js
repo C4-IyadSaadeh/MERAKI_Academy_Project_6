5396b269f31200835fd115791fbb30da40b163d4
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2022-03-16 22:46:40 +0300

    Add Route and Redux provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from "react-redux";
+import store from "./Components/Redux/index";
+import { BrowserRouter  } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+   <Provider store={store}>
     <App />
-  </React.StrictMode>,
+
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
